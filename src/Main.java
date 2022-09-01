3ce5a39761da57b7f45b0d2f4235b2dae55de620
Vakulenko <og4youforever@gmail.com>
2022-09-01 16:07:15 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
+      //Задача 1
+      //int [] number = new int [3];
+      //double [] numberDouble = {1.57, 7.654, 9.986};
+      //double [] sugar = {5.34};
+
+
+      //Задание 2
+       // int [] ints = new int [3];
+       // ints [0] = 1;
+       // ints [1] = 2;
+       // ints [2] = 3;
+       // System.out.println(ints [0]);
+       // System.out.println(ints [1]);
+       // System.out.println(ints [2]);
+      //  double [] numberDouble = {1.57, 7.654, 9.986};
+       // System.out.println(Arrays.toString(numberDouble));
+       // float [] floats = new float [3];
+      //  floats[0] = 15;
+       // floats[1] = 12;
+      //  floats[2] = 44;
+       // System.out.println(floats [0]);
+       // System.out.println(floats [1]);
+       // System.out.println(floats [2]);
+
+        //Задание 3
+
+        int [] ints = new int [3];
+        ints [0] = 1;
+        ints [1] = 2;
+        ints [2] = 3;
+        System.out.println(ints [2]);
+        System.out.println(ints [1]);
+        System.out.println(ints [0]);
+        double [] numberDouble = {9.986, 7.654, 1.57};
+        System.out.println(Arrays.toString(numberDouble));
+        float [] floats = new float [3];
+        floats[0] = 15;
+        floats[1] = 12;
+        floats[2] = 44;
+        System.out.println(floats [2]);
+        System.out.println(floats [1]);
+        System.out.println(floats [0]);
     }
 }
